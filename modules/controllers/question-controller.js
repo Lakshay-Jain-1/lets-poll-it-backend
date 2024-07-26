a79ec9c5c7b0a40934361c0df2c7673e5cd5f2c9
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-07-26 22:15:59 +0530

    Added new Callback functions

--- modules/controllers/question-controller.js
+++ modules/controllers/question-controller.js
@@ -3,11 +3,21 @@
 
 import { ProductModel } from "../models/question-schema.js"
 
-const  showAllQuestions = async(req,res)=>{
+const  showAllQuestions = async (req,res)=>{
    const data = await ProductModel.find({})
    res.send(data)
 }
 
+const makeAQuestion= async (req,res)=>{
+    const {question ,options,password} = req.body
+    const data = await ProductModel.create({question,options,password})
+    res.json({message:"question has been created"})
+ }
 
+const enterAQuestion = async (req,res)=>{
+   const {question,password} = req.body
+    const data = await ProductModel.find({question,password})
+    res.json(data)
+ }
 
-export {showAllQuestions}
+export {showAllQuestions, makeAQuestion ,enterAQuestion}
