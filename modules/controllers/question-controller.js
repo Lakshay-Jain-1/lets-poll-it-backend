2e1842c55967b838ee6970d27e52c090a4423e45
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-07-31 18:02:37 +0530

    Added new Routes

--- modules/controllers/question-controller.js
+++ modules/controllers/question-controller.js
@@ -1,32 +1,42 @@
-// route ke callback function ismein jaate hain 
+// route ke callback function ismein jaate hain
 // yahan par model ka use hoga
 
-import { ProductModel } from "../models/question-schema.js"
+import { ProductModel } from "../models/question-schema.js";
 
-const  showAllQuestions = async (req,res)=>{
-   const data = await ProductModel.find({})
-   console.log(data)
-   res.send(data)
-}
+const showAllQuestions = async (req, res) => {
+  const data = await ProductModel.find({});
 
-const makeAQuestion= async (req,res)=>{
-    const {question ,options,password} = req.body
-    const data = await ProductModel.create({question,options,password})
-    res.json({message:"question has been created"})
- }
+  res.send(data);
+};
 
-const enterAQuestion = async (req,res)=>{
-   const {question,password} = req.body
-   console.log(question,password)
-    const data = await ProductModel.find({question,password})
-    res.json(data)
- }
+const getAQuestion = async (req, res) => {
+  const { question } = req.body;
+  const data = await ProductModel.find({ question });
 
- const updateAPoll = async (req,res)=>{
-   const {question , option} = req.body
-   const newPolledEntry = {optionSelected: option };
-   const data = await ProductModel.findOneAndUpdate({question},{$push:{polled:newPolledEntry}})
-   res.json({message:"You have submited your poll"})
+  res.send(data);
+};
 
- }
-export {showAllQuestions, makeAQuestion ,enterAQuestion ,updateAPoll}
+const makeAQuestion = async (req, res) => {
+  const { question, options, password } = req.body;
+  const data = await ProductModel.create({ question, options, password });
+  res.json({ message: "question has been created" });
+};
+
+const enterAQuestion = async (req, res) => {
+  const { question, password } = req.body;
+
+  const data = await ProductModel.find({ question, password });
+  res.json(data);
+};
+
+const updateAPoll = async (req, res) => {
+  const { question, option } = req.body;
+  const newPolledEntry = { optionSelected: option };
+  const data = await ProductModel.findOneAndUpdate(
+    { question },
+    { $push: { polled: newPolledEntry } }
+  );
+  res.json({ message: "You have submited your poll" });
+};
+
+export { showAllQuestions, makeAQuestion, enterAQuestion, updateAPoll ,getAQuestion };
--- modules/routes/question-route.js
+++ modules/routes/question-route.js
@@ -1,13 +1,13 @@
 // yahan par route handling hoti hain
 
 import express from "express"
-import { enterAQuestion, makeAQuestion, showAllQuestions, updateAPoll } from "../controllers/question-controller.js"
+import { enterAQuestion, getAQuestion, makeAQuestion, showAllQuestions, updateAPoll } from "../controllers/question-controller.js"
 
 const router = express.Router()
 
 
 router.get("/",showAllQuestions)
-
+router.get("/get",getAQuestion)
 router.post("/generate",makeAQuestion)
 
 router.post("/enter",enterAQuestion)
