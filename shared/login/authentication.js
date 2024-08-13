1e97117296ece89b1a2bade312ed172a1fb6c9b9
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-08-14 00:40:33 +0530

    Identified the issue

--- shared/login/authentication.js
+++ shared/login/authentication.js
@@ -14,14 +14,14 @@ loginRouter.post("/",async(req,res)=>{
     let user_data = await UserModel.find({name,password})
     console.log(user_data)
     if(user_data.length==0){
-      res.cookie("success",false,{secure:true,sameSite:"none",expires:new Date(Date.now() +  9000), path:"/"}).json({"success":false})
+      res.cookie("success",false,{secure:true,sameSite:"none",expires:new Date(Date.now() +  10*9000), path:"/"}).json({"success":false})
     }else{
     let token =  generateToken(user_data)
-      res.cookie("token",token,{secure:true,sameSite:"none",expires:new Date(Date.now() +  9000),path:"/"}).cookie("success",true,{secure:true,sameSite:"none"}).json({"success":true})
+      res.cookie("token",token,{secure:true,sameSite:"none",expires:new Date(Date.now() +  10*9000),path:"/"}).cookie("success",true,{secure:true,sameSite:"none",path:"/"}).json({"success":true})
     }
     }catch(err){
       console.log("failed")
-          res.cookie("success",false,{secure:true,sameSite:"none",expires:new Date(Date.now() +  9000),path:"/"}).json({"success":false})
+          res.cookie("success",false,{secure:true,sameSite:"none",expires:new Date(Date.now() +  10*9000),path:"/"}).json({"success":false})
     }
 
 })
