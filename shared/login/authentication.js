08c130451c551ca4f5d09599cf9ad2a8cb968036
Lakshay Jain <lakshaybusiness.12@gmail.com>
2025-07-16 14:13:17 +0530

    Feat:Storing hashpassword rather raw password

--- shared/login/authentication.js
+++ shared/login/authentication.js
@@ -1,31 +1,53 @@
-import express from "express"
-import { UserModel } from "../../modules/models/user-schema.js"
-import { generateToken } from "../Token/token.js"
+import express from "express";
+import { UserModel } from "../../modules/models/user-schema.js";
+import { generateToken } from "../Token/token.js";
+import bcrypt from "bcrypt";
+const loginRouter = express.Router();
 
-const loginRouter = express.Router()
+// schema mein user and password se
 
-// schema mein user and password se 
-
-
-
-loginRouter.post("/",async(req,res)=>{
-    try{
-    const {name,password}= req.body
-    let user_data = await UserModel.find({name,password})
-    console.log(user_data)
-    if(user_data.length==0){
-      res.cookie("success",false,{secure:true,sameSite:"none",expires:new Date(Date.now() +  10*9000), path:"/"}).json({"success":false})
-    }else{
-    let token =  generateToken(user_data)
-      res.cookie("token",token,{secure:true,sameSite:"none",expires:new Date(Date.now() +  10*9000),path:"/"}).cookie("success",true,{secure:true,sameSite:"none",path:"/"}).json({"success":true})
+loginRouter.post("/", async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    let user_data = await UserModel.findOne({ name });
+    if (!user_data || !bcrypt.compareSync(password, user_data.password)) {
+    throw new Error("Invalid credentials");
     }
-    }catch(err){
-      console.log("failed")
-          res.cookie("success",false,{secure:true,sameSite:"none",expires:new Date(Date.now() +  10*9000),path:"/"}).json({"success":false})
+    if (user_data.length == 0) {
+      res
+        .cookie("success", false, {
+          secure: true,
+          sameSite: "none",
+          expires: new Date(Date.now() + 10 * 9000),
+          httpOnly:true,
+          path: "/",
+        })
+        .json({ success: false });
+    } else {
+      let token = generateToken({ name: user_data.name, _id: user_data._id });
+      res
+        .cookie("token", token, {
+          secure: true,
+          sameSite: "none",
+          httpOnly:true,
+          expires: new Date(Date.now() + 10 * 9000),
+          path: "/",
+        })
+        .cookie("success", true, { secure: true, sameSite: "none", path: "/" })
+        .json({ success: true });
     }
+  } catch (err) {
+    console.log("failed");
+    res
+      .cookie("success", false, {
+        secure: true,
+        sameSite: "none",
+        expires: new Date(Date.now() + 10 * 9000),
+        httpOnly:true,
+        path: "/",
+      })
+      .json({ success: false });
+  }
+});
 
-})
-
-
-
-export default loginRouter
+export default loginRouter;
--- shared/signup/signup.js
+++ shared/signup/signup.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { generateToken } from "../Token/token.js";
 import { UserModel } from "../../modules/models/user-schema.js";
-
+import bcrypt from "bcrypt";
 const signupRouter = express.Router();
 
 signupRouter.post("/", async (req, res) => {
   try {
     let { name, password } = req.body;
-    console.log(name,password)
-    await UserModel.create({ name, password });
-    let token = generateToken(name)
+    const hash = bcrypt.hashSync(password, 10);
+    const newUser = await UserModel.create({ name, password: hash });
+    let token = generateToken({ name: newUser.name, _id: newUser._id });
     res.cookie("token",token ,{secure:true,sameSite:"none"} ).cookie("success",true,{secure:true,sameSite:"none"}).json({ success: true });
   } catch (err) {
     res.cookie("success",false,{secure:true,sameSite:"none"}).json({ success: false });
